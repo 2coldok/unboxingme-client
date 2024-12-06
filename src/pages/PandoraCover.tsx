d5796f329f9d5e0a4132819642e0bc648f6fff2c
coldok <2coldok@gmail.com>
2024-12-06 23:39:48 +0900

    fix: react-router와 충돌하지 않도록 location.href 를 통해 페이지 이동

--- src/pages/PandoraCover.tsx
+++ src/pages/PandoraCover.tsx
@@ -59,7 +59,7 @@ export default function PandoraCover() {
       const status = await getTokenStatus();
       if (!status) {
         // return setShowLoginPop(true);
-        return navigate(`/login?redirect=${encodeURIComponent(window.location.href)}`);
+        window.location.href = `https://riddlenote.com/login?redirect=${encodeURIComponent(window.location.href)}`;
       }
 
       return navigate(`/pandora/${id}/riddle`);
